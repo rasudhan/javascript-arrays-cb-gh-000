bbf0c81d786d5cb357a864daefe8cba3efe9988c
Anandha Sudhan <rasudhan@gmail.com>
2018-10-04 16:24:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,7 @@ function addElementToBeginningOfArray(array,element) {
   return arr
 }
 
-function destructivelyAddElementToBeginningOfArray() {
-
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  array.unshift(element)
+  return array
 }

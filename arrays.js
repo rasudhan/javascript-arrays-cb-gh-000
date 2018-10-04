c2424f2fe8585ae1f713d9b2b572dcb14d49c44c
Anandha Sudhan <rasudhan@gmail.com>
2018-10-04 16:29:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   return array
 }
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1)
+  return array
+}
